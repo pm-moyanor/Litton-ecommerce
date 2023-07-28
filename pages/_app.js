9d2ccd6813c4829cb102917e3efc744bf47aeb00
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2023-07-28 15:53:44 -0400

    fix google-font

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
+import "../styles/globals.css";
 
-import { Oswald_700Latin } from '@fontsource/oswald';
-import '../styles/globals.css'
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCube, faHeart, faShoppingCart, faShop, faTruckFast, faUser } from '@fortawesome/free-solid-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCube,
+  faHeart,
+  faShoppingCart,
+  faShop,
+  faTruckFast,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-
-library.add(faTruckFast,faShop,faCube)
+library.add(faTruckFast, faShop, faCube);
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div >
+      <Component {...pageProps} />
+    </div>
+  );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      {/* <link
+            href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;600;800&display=swap"
+            rel="stylesheet"
+          /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
